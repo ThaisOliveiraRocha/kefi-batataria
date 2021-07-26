06bc45d7fe82a15b3928767ca8936baa3ecc3e4a
Thais Rocha <thais.rocha@dextra-sw.com>
2021-07-26 18:44:58 -0300

    Create page Home

---
+++ cliente/src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { Test } from './Home.styles';
+
+const Home = () => (
+  <div>
+    <div>Menu</div>
+    <div>Banner</div>
+    <div>Cardapio</div>
+    <div>Embalagens</div>
+    <div>Horta</div>
+    <div>Galeria</div>
+    <div>Footer</div>
+  </div>
+);
+
+export default Home;

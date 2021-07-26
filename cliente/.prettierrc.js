6561953572919968c38d13daa7f64fd689dcceed
Thais Rocha <thais.rocha@dextra-sw.com>
2021-07-26 18:42:10 -0300

    Setup lint

---
+++ cliente/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'es5',
+  'editor.formatOnSave': true,
+  'editor.formatOnPaste': true,
+  '[yaml]': {
+    'editor.formatOnSave': false,
+    'editor.formatOnPaste': false,
+  },
+};
